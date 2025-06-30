2793b0e0a537c1f247fa2734723fb4abe71d0cce
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-06-30 13:18:42 +0200

    header and app update

--- by-the-pods/src/components/Header.jsx
+++ by-the-pods/src/components/Header.jsx
@@ -4,7 +4,12 @@ import Link from "react-router-dom";
 const Header = () => {
     return (
         <header>
-            
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+            </nav>
         </header>
     )
 }
+
+export default Header
