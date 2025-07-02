c1473ff2a05d4c2d4946f67e312928e4f30b7979
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-07-02 17:52:31 +0200

    update

--- by-the-pods/src/App.jsx
+++ by-the-pods/src/App.jsx
@@ -3,14 +3,23 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
+import { data } from 'react-router-dom'
 
-function App() {
+const App = () => {
+React.useEffect(() => {
+  fetch ("https://podcast-api.netlify.app")
+  .then((response)=>response.json())
+  .then((data) => console.log(data));
+}, [])
 
-  return ( 
-      <Header>
-      Open Space for files and things to  throw in 
-      </Header>
-  )
-}
+return (
+  <div>
+    <Header />
+  </div>
+)
+
+} 
+
+
 
 export default App
