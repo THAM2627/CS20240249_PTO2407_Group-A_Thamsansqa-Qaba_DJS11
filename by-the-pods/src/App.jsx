afd27c68a038c94bf3a2c23b5b82285d2a9716f0
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-07-03 18:17:03 +0200

    Update App.jsx

--- by-the-pods/src/App.jsx
+++ by-the-pods/src/App.jsx
@@ -1,9 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react';
 import './App.css'
 import Header from './components/Header'
-import { data } from 'react-router-dom'
 import Footer from './components/Footer'
 
 const App = () => {
