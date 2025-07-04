02703150769f218f9b2e70688c54e6f30d4684d2
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-07-04 14:11:55 +0200

    Update Footer.jsx

--- by-the-pods/src/components/Footer.jsx
+++ by-the-pods/src/components/Footer.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-import Link from "react-router-dom";
+import {Link} from "react-router-dom";
 
 const Footer = () => {
     return (
+
         <footer>
-            
+             <p>
+            Hello Some text from the footer to check the components.
+        </p>
+        
+            <Link to = "about">About</Link>
         </footer>
     )
 }
