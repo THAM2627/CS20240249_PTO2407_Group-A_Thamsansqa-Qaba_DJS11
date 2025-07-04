2818ba57c4a45f2bb6cad0db928993beb4a6f142
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-07-04 14:11:57 +0200

    Update Header.jsx

--- by-the-pods/src/components/Header.jsx
+++ by-the-pods/src/components/Header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import Link from "react-router-dom";
+import {Link} from "react-router-dom";
 
 const Header = () => {
     return (
         <header>
             <nav>
+                <p>
+                    Hello Some text from the header to check the components.
+                </p>
                 <Link to="/">Home</Link>
                 <Link to="/about">About</Link>
             </nav>
