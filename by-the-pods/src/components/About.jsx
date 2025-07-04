f1c5b5c9db4e0ecf2d7682cb02ca26efdff49675
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-07-04 15:16:11 +0200

    Update About.jsx

--- by-the-pods/src/components/About.jsx
+++ by-the-pods/src/components/About.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
-export default function About() {
+const About = () => {
     return (
         <div>
-            <h1>About</h1>
+            <h1>About US</h1>
             <h2>BY THE PODS</h2>
             <p>
-                
+            Welcome to By The Pods. We are a podcast app that allows you to listen to your favorite podcasts. Our app is designed to be easy to use, easy on the eyes, and easy on the ears. 
             </p>
         </div>
     );
 }
+
