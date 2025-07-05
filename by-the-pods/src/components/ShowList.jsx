b94cb890bc5a798a6c0fcb0cf9b547d1bd54c3dc
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-07-05 19:37:06 +0200

    Update ShowList.jsx

--- by-the-pods/src/components/ShowList.jsx
+++ by-the-pods/src/components/ShowList.jsx
@@ -7,8 +7,31 @@ const ShowList = () => {
 
 useEffect(() => {
     const fetchShows = async () => {
-        
-    }
-}
+        const response = await fetch("https://podcast-api.netlify.app");
+        const data = await response.json();
+        setShows(data);
+        setLoading(false);
+    };
+    fetchShows();
+}, []);
 
-}
+return (
+    <div className="show-list">
+        {loading ? (
+            <p>Loading...</p>
+        ):(
+            <div className="show-grid">
+                {shows.map((show) => (
+                    <div key={show.id} className="show-card">
+                <h2>{show.title}</h2>
+                <img src="show.image" alt={show.title} />
+                <p>Genres: {show.genres.join(", ")}</p>
+                </div>
+                ))}
+            </div>
+        )}
+    </div>
+);
+};
+
+export default ShowList
