a1507465d6881805d7651546550890734e08129d
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-07-07 00:16:17 +0200

    Update Favourites.jsx

--- by-the-pods/src/components/Favourites.jsx
+++ by-the-pods/src/components/Favourites.jsx
@@ -4,6 +4,10 @@ const Favourites = () => {
     return (
         <div>
             <h1>Favourites</h1>
+            <ul>
+            {favourites.map((podcast) => ( <li key={podcast.id}>{podcast.title}</li> 
+            ))}
+            </ul>
         </div>
     );
 };
