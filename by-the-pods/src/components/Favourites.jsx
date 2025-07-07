0703f4ad3ed60b7d0fb3c7fd50b97de84b2df011
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-07-07 20:31:00 +0200

    Update Favourites.jsx

--- by-the-pods/src/components/Favourites.jsx
+++ by-the-pods/src/components/Favourites.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { Favourites } from "./FavouritesFunction";
+import FavouritesContext from "./FavouritesFunction";
 
 const FavouritesList = () => {
     const {favourites } = useContext(FavouritesContext);
