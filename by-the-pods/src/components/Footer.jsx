6a781d3d880cc50f45d9443418386ff45f567cc5
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-07-09 00:16:58 +0200

    Update Footer.jsx

--- by-the-pods/src/components/Footer.jsx
+++ by-the-pods/src/components/Footer.jsx
@@ -1,13 +1,15 @@
 import React, { useRef, useState } from "react";
+import { useNowPlaying } from "./NowPlayingGlobal";
 
 
 const Footer = () => {
-    const [isPlaying, setIsPlaying] = useState(false);
-    const audioRef = useRef(null);
+    const [isPlaying, setIsPlaying] = useNowPlaying();
+    const audioRef = useState(false);
 
     const togglePlay = () => {
         const audio = audioRef.current;
         if (!audio) return;
+
         if (isPlaying) {
             audio.pause();
         } else {
@@ -16,27 +18,21 @@ const Footer = () => {
         setIsPlaying(!isPlaying);
     };
 
+    if(!episode) return null;
+
     return (
         <footer className="fixed bottom-0 w-full bg-gray-900 text-white px-4 py-3 shadow-lg z-50">
         <div  className="flex flex-col sm:flex-row items-center justify-between">
             {/* Audio Player - Now Playing */}
         <div className="flex items-center gap-4 mb-2 sm:mb:0">
         <img 
-        src = {show.image}
-        alt = {show.title}
+        src = {episode.image}
+        alt = "cover"
         classname = "w-10 h-10 rounded-lg"/>
 
-        <div className="flex items-center gap-4">
-            <button
-            onClick = {togglePlayback}
-            className="bg-white text-black px-3 py-1 rounded hover:bg-gray-300 transition">
-            {isPlaying ? "Pause" : "Play"}
-            </button>
-        {/* Audio Elements */}
-        <audio
-        ref = {audioRef}
-        src = {episode.audioUrl}/>
-
+        <div> 
+        <p className="text-sm font-semibold truncate max-w-[150px]">{episode.title}</p>
+        <p className="text-xs text-gray-400 truncate max-w-[150px]">{episode.show || "Unknown Show"}</p> 
         </div>
         </div>
             </div>
