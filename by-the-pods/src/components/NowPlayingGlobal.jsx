baa5dba30ce870d3c9799f2ebda9d9c7ef44b54d
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-07-08 23:56:20 +0200

    playing episode function

--- by-the-pods/src/components/NowPlayingGlobal.jsx
+++ by-the-pods/src/components/NowPlayingGlobal.jsx
@@ -1,1 +1,19 @@
 import React, { createContext, useState, useContext, useRef } from "react";
+
+const NowPlayingContext = createContext();
+
+export const NowPlayingProvider = ({ children }) => {
+    const [episode, setEpisode] = useState(null);
+    const audioRef = useRef(null);
+
+const playEpisode = (episodeData) => {
+    setEpisode(episodeData);
+    setTimeout(() => {
+        if (audioRef.current) {
+            audioRef.current.play();
+        }
+    },100);
+};
+
+const 
+}
