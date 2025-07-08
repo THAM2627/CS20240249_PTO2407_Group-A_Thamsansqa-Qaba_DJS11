173abb8c26f08f2958dd2de30737fe36203079db
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-07-09 00:47:37 +0200

    Update Favourites.jsx

--- by-the-pods/src/components/Favourites.jsx
+++ by-the-pods/src/components/Favourites.jsx
@@ -7,11 +7,10 @@ const FavouritesList = () => {
     return (
         <div className="bg-black text-white min-h-screen px-4 py-8">
             <h1 className="text-3xl font-bold mb-6 text-center">❤️Your Favourites❤️</h1>
-            {favourites}
-            <ul>
-            {favourites.map((podcast) => ( <li key={podcast.id}>{podcast.title}</li> 
+            {favourites.map((podcast) => (
+                
             ))}
-            </ul>
+           
         </div>
     );
 };
