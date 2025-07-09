86839302a6ececf1a1838d2aa7ac563502498a70
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-07-09 23:27:04 +0200

    Update NowPlayingGlobal.jsx

--- by-the-pods/src/components/NowPlayingGlobal.jsx
+++ by-the-pods/src/components/NowPlayingGlobal.jsx
@@ -5,6 +5,8 @@ const NowPlayingContext = createContext();
 export const NowPlayingProvider = ({ children }) => {
     const [episode, setEpisode] = useState(null);
     const [isPlaying, setIsPlaying] = useState(false);
+    const [ currentTime, setCurrentTime ] = useState(0);
+    const [ duration, setDuration ] = useState(0);
     const audioRef = useRef(null);
 
 const playEpisode = (episodeData) => {
@@ -32,7 +34,7 @@ const play = () => {
 };
 
 return (
-    <NowPlayingContext.Provider value={{ episode, playEpisode, pause, play, audioRef, isPlaying, setIsPlaying }}>
+    <NowPlayingContext.Provider value={{ episode, playEpisode, pause, play, audioRef, isPlaying, setIsPlaying, currentTime, setCurrentTime, duration, setDuration  }}>
         {children}
     </NowPlayingContext.Provider>
 );
