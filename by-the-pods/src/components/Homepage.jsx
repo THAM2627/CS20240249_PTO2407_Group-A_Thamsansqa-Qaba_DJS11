ca83d716100320b50dcd75c0e386f8a4331f3ce4
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-07-09 14:16:47 +0200

    Update Homepage.jsx

---
+++ by-the-pods/src/components/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Homepage = () => {
+    return(
+        <div className="text-center mt-20">
+        <h1 className="text-3xl font-bold mb-4">Welcome to By The Pods</h1>
+        <p className="text-lg text-gray-300  mb-6 max-w-xl mx-auto">
+            Discover your favourite podcasts and start listening to them now!
+        </p>
+        </div>
+    )
+}
