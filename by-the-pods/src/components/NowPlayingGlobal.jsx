3439142a3716d1699e89978a3f0fce6535d5b6a8
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-07-10 00:16:56 +0200

    Update NowPlayingGlobal.jsx

--- by-the-pods/src/components/NowPlayingGlobal.jsx
+++ by-the-pods/src/components/NowPlayingGlobal.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useContext, useRef } from "react";
+import React, { createContext, useState, useContext, useRef, useEffect } from "react";
 
 const NowPlayingContext = createContext();
 
