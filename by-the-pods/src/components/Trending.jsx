242a4f41e27406c076ba34c0129dd71a5429297d
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-07-09 14:12:19 +0200

    Update Trending.jsx

--- by-the-pods/src/components/Trending.jsx
+++ by-the-pods/src/components/Trending.jsx
@@ -27,3 +27,29 @@ useEffect(() => {
     })();
 }, []);
 
+return (
+    <div className="mt-12">
+        <h2 className="text-3xl font-bold mb-4">Trending Podcasts</h2>
+        <div className="flex space-x-4 overflow-x-auto pb-4">
+        {trending.map((podcast) => (
+            <Link
+            key={show.id}
+            to="/Shows"
+            className="min-w-[150px] flex-shrink-0 bg-gray-800 rounded-lg shadow-md hover:scale-105 transition transform} ">
+            <img 
+            src={show.image}
+            alt={show.title}
+            className="w-full h-40 sm:h-32 md:h-36 lg:h-44 object-cover"/>
+            <div className="p-4">
+            <p className="text-sm md:text-base font-bold truncate">{show.title}</p>
+            <p className="text-xs text-white">
+                {genreMap[show.genreIds[0]]||"Podcast"}
+                </p>
+                </div>
+            </Link>
+        ))}
+        </div>
+    </div>
+)
+
+e
