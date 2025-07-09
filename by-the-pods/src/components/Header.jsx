fd1685cf54c1b5173c48d39288ae3c099b2b774e
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-07-09 13:15:17 +0200

    Update Header.jsx

--- by-the-pods/src/components/Header.jsx
+++ by-the-pods/src/components/Header.jsx
@@ -4,9 +4,10 @@ import {Link} from "react-router-dom";
 const Header = () => {
     return (
         
-        <header className="bg-blue-500 text-black p-4 md:p-6 lg:p-8">
-        <h1 className="text-2xl font-bold">By The Pods</h1>"
-            <nav>
+        <header className="bg-gray-900 text-white px-6 py-4 md:px-12 md:py-6 shadow-md sticky top-0 z-50">
+        <h1 className="text-2xl font-extrabold tracking-tight cursor-default select-none">By The Pods</h1>
+
+            <nav className="flex space-x-6 ">
                 <Link to="/">Home</Link>
                 <Link to="/About">About</Link>
                 <Link to="/Shows">Shows</Link>
