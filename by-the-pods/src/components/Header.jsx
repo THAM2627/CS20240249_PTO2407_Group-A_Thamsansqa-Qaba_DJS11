37ea0035c47c1d977bbe62fbdc078ada9f043b1f
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-07-09 13:21:21 +0200

    header syling

--- by-the-pods/src/components/Header.jsx
+++ by-the-pods/src/components/Header.jsx
@@ -7,11 +7,15 @@ const Header = () => {
         <header className="bg-gray-900 text-white px-6 py-4 md:px-12 md:py-6 shadow-md sticky top-0 z-50">
         <h1 className="text-2xl font-extrabold tracking-tight cursor-default select-none">By The Pods</h1>
 
-            <nav className="flex space-x-6 ">
-                <Link to="/">Home</Link>
-                <Link to="/About">About</Link>
-                <Link to="/Shows">Shows</Link>
-                <Link to="/Favourites">Favourites</Link>
+            <nav className="flex space-x-6 text-sm font-medium">
+                <Link to="/"
+                className="hover:text-red-500 transition-colors duration-300">Home</Link>
+                <Link to="/About"
+                className="hover:text-red-500 transition-colors duration-300">About</Link>
+                <Link to="/Shows"
+                className="hover:text-red-500 transition-colors duration-300">Shows</Link>
+                <Link to="/Favourites"
+                className="hover:text-red-500 transition-colors duration-300">Favourites</Link>
             </nav>
         </header>
 
