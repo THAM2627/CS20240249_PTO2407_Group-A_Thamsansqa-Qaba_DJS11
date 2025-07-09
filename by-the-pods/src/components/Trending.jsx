b9ab48500e1245437b913290a26898c8cdf36aa9
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-07-09 18:36:55 +0200

    Update Trending.jsx

--- by-the-pods/src/components/Trending.jsx
+++ by-the-pods/src/components/Trending.jsx
@@ -15,16 +15,22 @@ const genreMap = {
 
 const Trending = () => {
     const [trending, setTrending] = useState([]);
-}
+
 
 useEffect(() => {
-    (async () => {
+    const fetchTrending = async () => {
+        try {
         const resp = await fetch ("https://podcast-api.netlify.app");
         const data = await resp.json();
         const sorted = data.sort((a,b) => new Date(b.updated) - new Date(a.updated))
         .slice(0,5);
         setTrending(sorted);
-    })();
+    } catch (err) {
+        console.error("Error fetching trending",err);
+    };
+}
+
+    fetchTrending();
 }, []);
 
 return (
@@ -33,17 +39,17 @@ return (
         <div className="flex space-x-4 overflow-x-auto pb-4">
         {trending.map((podcast) => (
             <Link
-            key={show.id}
+            key={podcast.id}
             to="/Shows"
             className="min-w-[150px] flex-shrink-0 bg-gray-800 rounded-lg shadow-md hover:scale-105 transition transform} ">
             <img 
-            src={show.image}
-            alt={show.title}
+            src={podcast.image}
+            alt={podcast.title}
             className="w-full h-40 sm:h-32 md:h-36 lg:h-44 object-cover"/>
             <div className="p-4">
-            <p className="text-sm md:text-base font-bold truncate">{show.title}</p>
+            <p className="text-sm md:text-base font-bold truncate">{podcast.title}</p>
             <p className="text-xs text-white">
-                {genreMap[show.genreIds[0]]||"Podcast"}
+                {genreMap[podcast.genreIds[0]]||"Podcast"}
                 </p>
                 </div>
             </Link>
@@ -51,5 +57,6 @@ return (
         </div>
     </div>
 )
+        }
 
 export default Trending
