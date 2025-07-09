90d5f8597463b96cdcdda9391a098c3c429a1e76
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-07-09 13:58:51 +0200

    Create Trending.jsx

---
+++ by-the-pods/src/components/Trending.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const genreMap = {
+    1:"Personal Growth", 
+    2:"Investigative Journalism", 
+    3:"History", 
+    4:"Comedy", 
+    5:"Entertainment", 
+    6:"Business", 
+    7:"Fiction", 
+    8:"News", 
+    9:"Kids and Family"
+}
