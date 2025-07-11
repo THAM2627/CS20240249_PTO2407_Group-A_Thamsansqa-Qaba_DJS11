157d5cef24883f622ff18532300a9095a779d7ab
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-07-11 19:59:46 +0200

    Update NowPlayingGlobal.jsx

--- by-the-pods/src/components/NowPlayingGlobal.jsx
+++ by-the-pods/src/components/NowPlayingGlobal.jsx
@@ -12,6 +12,7 @@ export const NowPlayingProvider = ({ children }) => {
 // Function to play an episode
 const playEpisode = (episodeData) => {
     setEpisode(episodeData);
+    console.log("Playing episode:", episodeData);
 };
 
 // Function to pause the audio
